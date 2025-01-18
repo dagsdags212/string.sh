ace7f3a9d85be6623168401317e8ab0fe9eb47ad
dagsdags212 <jegsamson.dev@gmail.com>
2025-01-19 05:15:21 +0800

    addded GenericError and NoMatchError

--- lib/error_codes.sh
+++ lib/error_codes.sh
@@ -3,8 +3,10 @@
 declare -A ERRORS
 
 SUCCESS=0
+ERRORS[GenericError]=1
 ERRORS[IndexError]=10
 ERRORS[ValueError]=11
+ERRORS[NoMatchError]=12
 
 display_error_codes() {
   for i in "${!ERRORS[@]}"; do
@@ -15,6 +17,10 @@ display_error_codes() {
 raise() {
   local err_type=$1
   local msg=$2
+  if [[ "${err_type}" == GenericError ]]; then
+    printf "%s: %s\n" "${err_type}" "an exception was raised" 1>&2
+    exit ${ERRORS[err_type]}
+  fi
   printf "%s: %s\n" "${err_type}" "${msg}" 1>&2
   exit ${ERRORS[err_type]}
 }
